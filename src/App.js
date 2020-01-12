8142cce702a14997807f1df2273bcb177b8163c9
Chaitanya Mahajan <chatumahajan07@gmail.com>
2020-01-12 23:27:38 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import SearchForm from './Components/SearchForm'
+
+export default class App extends Component{
+
+    render(){
+        return(
+            <div className = "App">
+            <SearchForm />
+            </div>
+        )
+    }
+}
